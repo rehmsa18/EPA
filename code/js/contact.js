4ded68f4289767e3cf8d6fc609b075179f0f4bdd
rehmsa18 <61938425+rehmsa18@users.noreply.github.com>
2020-07-12 19:40:06 +0200

    Add files via upload

---
+++ code/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "le nom n'est pas indiqué",
+                    minlength: "le nom doit avoir au moins 2 caractères"
+                },
+                subject: {
+                    required: "le motif n'est pas indiqué",
+                    minlength: "le motif doit avoir au moins 4 caractères"
+                },
+                number: {
+                    required: "le numéro n'est pas indiqué",
+                    minlength: "le numéro doit avoir au moins 5 caractères"
+                },
+                email: {
+                    required: "l'email n'est pas indiqué"
+                },
+                message: {
+                    required: "le message n'est pas indiqué",
+                    minlength: "Ecrivez davantage"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
